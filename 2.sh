3220c47e748f9ba91b5ca292ea100562a755d587
Senmo-boop <18624963501@163.com>
2026-02-10 21:34:26 +0300

    Delete 2.sh

--- 2.sh
+++
@@ -1,6 +0,0 @@
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/rcan3x.py 1
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/rdn3x.py 1
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/real_esrgan3x.py 1
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/srcnn3x.py 1
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/srgan3x.py 1
-CUDA_VISIBLE_DEVICES=2 ./tools/dist_train.sh ./my_config_3x/swinoir3x.py 1
